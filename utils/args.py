5189d3a827b0772ffdbdb069eba16ad8790fc020
Vera Proiss <veraproiss@gmail.com>
2023-10-22 14:36:29 +0200

    edit args.py

--- utils/args.py
+++ utils/args.py
@@ -4,6 +4,6 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Hyperparameters for Grid Search")
     parser.add_argument('--learning-rates', nargs='+', type=float, default=[1e-2, 1e-4, 1e-6])
     parser.add_argument('--batch-sizes', nargs='+', type=int, default=[64, 128, 256])
-    parser.add_argument('--epochs', nargs='+', type=int, default=[10, 100, 150])
+    parser.add_argument('--epochs', nargs='+', type=int, default=[50, 100, 150])
     args = parser.parse_args()
     return args
