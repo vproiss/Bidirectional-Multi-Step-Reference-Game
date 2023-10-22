855874f246464f0293f34719e36c19f6761a17a1
Vera Proiss <veraproiss@gmail.com>
2023-10-23 00:22:59 +0200

    clean-up

--- utils/args.py
+++ utils/args.py
@@ -6,4 +6,4 @@ def parse_args():
     parser.add_argument('--batch-sizes', nargs='+', type=int, default=[64, 128, 256])
     parser.add_argument('--epochs', nargs='+', type=int, default=[50, 100, 150])
     args = parser.parse_args()
-    return args
+    return args  
