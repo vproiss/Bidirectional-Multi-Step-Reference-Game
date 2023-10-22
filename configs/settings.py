2388f2f7f18c963bba2cde7d3e1862db21a57c9d
Vera Proiss <veraproiss@gmail.com>
2023-10-22 13:19:20 +0200

    add configs

---
+++ configs/settings.py
@@ -0,0 +1,15 @@
+# Related to dataset.
+SUBSET_SIZE = 32 # It has to be at least of size 32.
+BATCH_SIZE = 128
+
+# Related to communication protocol and embedding.
+MESSAGE_LENGTH = 100
+NUM_MESSAGES = 1000
+EMBEDDING_SIZE = 128
+COM_FEATURE_SIZE = 128
+
+# Related to feature extraction.
+FEATURE_SIZE = 2048
+
+# Related to training.
+SCALING_FACTOR = 0.01
